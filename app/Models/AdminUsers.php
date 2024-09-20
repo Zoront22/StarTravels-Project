ab8c5d0e6f8df09bc310cb0b1110fb47064b4009
Osvaldo Najera Sandoval <165745030+Zoront22@users.noreply.github.com>
2024-09-20 10:48:43 -0600

    Register correct - Login PENDING...

--- app/Models/AdminUsers.php
+++ app/Models/AdminUsers.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AdminUsers extends Model
+class AdminUsers extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
 
     protected $table = "admin";
 
